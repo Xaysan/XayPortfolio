2f54f6b906c44ec5adb572999efe85cb62bd55bb
xaysanazz.k <huaweilaoss300@gmail.com>
2023-07-16 15:25:19 +0700

    add icon

--- basicvue/src/App.vue
+++ basicvue/src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="d-flex flex-row mb-3">
-  <div class="p-2">Flex item 1</div>
-  <div class="p-2">Flex item 2</div>
-  <div class="p-2">Flex item 3</div>
+  <div class="d-flex flex-column mb-3">
+  <div class="p-2">HELLO</div>
+  <div class="p-2">WELLCOM TO</div>
+  <div class="p-2">MY ROOM</div>
 </div>
-<div class="d-flex flex-row-reverse">
-  <div class="p-2">Flex item 1</div>
+<div class="d-flex flex-row mb-3">
+  <div class="p-2"><span class="material-symbols-outlined">
+person
+</span></div>
   <div lang="la" class="p-2">ສະບາຍດີ</div>
-  <div class="p-2">Flex item 3</div>
+  <div class="p-2"><span class="material-symbols-outlined">
+home
+</span></div>
 </div>
 </template>
 
